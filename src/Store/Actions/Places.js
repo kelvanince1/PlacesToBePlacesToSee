c7912064a209693e213d7fb975d2fda7df6228ec
Kelvan Ince <incekelvan@gmail.com>
2018-02-15 09:56:57 -0600

    Add fetch api to send data to Firebase

--- src/Store/Actions/Places.js
+++ src/Store/Actions/Places.js
@@ -1,11 +1,21 @@
 import { ADD_PLACE, DELETE_PLACE } from './ActionTypes';
+import { url } from './config';
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: ADD_PLACE,
-        placeName: placeName,
-        location: location,
-        image: image
+    return dispatch  => {
+        const placeData = {
+          name: placeName,
+          location: location
+        };
+        fetch(url, {
+          method: "POST",
+          body: JSON.stringify(placeData)
+        })
+          .catch(err => console.log(err))
+          .then(res => res.json())
+          .then(parsedRes => {
+            console.log(parsedRes);
+          })
     };
 };
 
--- src/Store/ConfigureStore.js
+++ src/Store/ConfigureStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import PlacesReducer from './Reducers/Places';
 
@@ -6,8 +7,14 @@ const rootReducer = combineReducers({
   places: PlacesReducer
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configureStore;
